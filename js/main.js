907bffd2a5f9f68f144760e862e92861ac277da5
faja rivaldi chan <69442735+fajar-dev@users.noreply.github.com>
2021-01-31 08:25:14 +0700

    update

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//typed js
+new Typed('#typed',{
+  strings : ['Fajar Rivaldi Chan'],
+  typeSpeed : 90,
+  delaySpeed : 120,
+  loop : true
+});
+
+//wa
+function whatsapp() {
+  window.location.href = "https://api.whatsapp.com/send?phone=62895611024559&text=Hallo";
+}
